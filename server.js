4f0efbd3db7601839dd2ca8c13c4b12fba958d6e
mmpotulo28 <mpotulom28@gmail.com>
2024-01-28 19:07:26 +0200

    change listneing post to 8080

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require( 'express' );
 const bodyParser = require( 'body-parser' );
 const app = express();
 const cors = require( 'cors' );
-const port = 3000;
+const port = 8080;
 
 app.use( cors() );
 
